d6b16c9c63ca8022df77a292d118fc42f40e63cf
Ilya <snacksncode@gmail.com>
2020-10-15 20:05:33 +0200

    Fix no response

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import Discord, { TextChannel } from 'discord.js';
 import { token } from './config';
 import parseMessage from './utils/parseMessage';
+import express from 'express';
+
 const client = new Discord.Client();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Works?");
+})
+
+app.listen(process.env.PORT || 3000);
 
 client.on('ready', () => {
   console.log(`\x1b[32m[ Ready ]\x1b[0m Logged in as \x1b[34m${client.user.tag}!`);
