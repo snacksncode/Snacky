0ecaa0b626d0508bcdf44eb8220ad77cb9093c5a
Ilya <snacksncode@gmail.com>
2020-10-15 19:52:17 +0200

    Work pls?

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,3 @@
-import * as fs from 'fs';
-let configFileDir = `./config.json`
-
-let fileExists = fs.existsSync(configFileDir);
-let data: string = (fileExists) ? fs.readFileSync(configFileDir, 'utf-8') : "{token:null}";
-
-export let token: string = JSON.parse(data).token || process.env.TOKEN;
+export let token: string = process.env.TOKEN;
 export let prefix: string = "s!";
 export let version: string = "1.2 Beta"
