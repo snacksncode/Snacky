feadf5dde7ccf4435448259fdabd4a65cba3644f
Ilya <snacksncode@gmail.com>
2020-10-19 00:08:02 +0200

    Dices convertion

--- src/commands/rollDice.ts
+++ src/commands/rollDice.ts
@@ -3,29 +3,50 @@ import { prefix } from "../config";
 import getRandomInt from "../utils/getRandomInt";
 import outputEmbedMessage from "../utils/outputEmbedMessage";
 
-const rollDice = (msg: Message, userInput: string, args: string[], channel: TextChannel) => {
+interface diceObject {
+  amount: number;
+  dice: number;
+}
+
+const convertDices = (dices: string[]): diceObject[] => {
+  let convertedDices: diceObject[] = [];
+  dices.forEach((dice: string) => {
+    let destructuredDice: number[] = dice
+      .split("d")
+      .map((dice) => Number(dice));
+    convertedDices.push({
+      amount: isNaN(destructuredDice.shift()) ? 1 : destructuredDice.shift(),
+      dice: destructuredDice.shift(),
+    });
+  });
+  return convertedDices;
+};
+
+const rollDice = (
+  msg: Message,
+  userInput: string,
+  args: string[],
+  channel: TextChannel
+) => {
   // let randomChance: number = getRandomInt(1, 6);
   let dices: string[] = userInput.match(/\d?d\d{1,3}/g);
   let hasHelpFlag: boolean = !!userInput.match(/--help/g);
   if (hasHelpFlag) {
-    outputEmbedMessage(
-      `Help for the thing coming right up`,
-      msg,
-      'info'
-    )
+    outputEmbedMessage(`Help for the thing coming right up`, msg, "info");
     return;
   } else if (dices === null) {
     outputEmbedMessage(
       `Sorry I couldn't find any valid dices in your message. Try using **${prefix}rollDice --help**`,
       msg,
-      'error'
-    )
+      "error"
+    );
     return;
   }
+  let dicesArray: diceObject[] = convertDices(dices);
   outputEmbedMessage(
-    `Here are what dices I've detected: ${dices}`,
+    `Detected and converted dices: ${dicesArray}`,
     msg,
-    'success'
-  )
-}
+    "success"
+  );
+};
 export default rollDice;
