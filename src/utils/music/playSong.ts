c8a66791db94cfffff3639d02c3a2211731e7afe
Ilya <snacksncode@gmail.com>
2020-12-10 03:14:19 +0100

    Added partial support for livestreams

--- src/utils/music/playSong.ts
+++ src/utils/music/playSong.ts
@@ -7,9 +7,11 @@ import { colors } from "../../config";
 async function playSong(msg: Message, song: Song) {
   const guild = msg.guild;
   const guildQueue = getQueue(guild.id, guild.client);
-  //function will call itself recursively so after we play the last song
-  //it'll attempt to play song at guildQueue.songs[0] which will be undefined
-  //it means that we have gone through the queue and can leave the channel
+  /*
+    function will call itself recursively so after we play the last song
+    it'll attempt to play song at guildQueue.songs[0] which will be undefined
+    it means that we have gone through the queue and can leave the channel
+  */
   if (!song) {
     guildQueue.isPlaying = false;
     guildQueue.voiceChannel.leave();
@@ -23,10 +25,16 @@ async function playSong(msg: Message, song: Song) {
   //get audioStream from ytdl
   try {
     let audioStream = ytdl(song.id, {
-      filter: "audioonly",
+      /*
+        audio only filter broke livestreams so it got yeeted.
+        livestreams still "buffer" a lot. Not really sure why
+      */
+      quality: "highestaudio",
       opusEncoded: true,
       encoderArgs: ["-af", "bass=g=10,dynaudnorm=f=200"],
+      highWaterMark: 1 << 25,
     });
+    //play the audioStream and repeatedly call itself
     const voiceChannelDispatcher = guildQueue.connection
       .play(audioStream, {
         type: "opus",
