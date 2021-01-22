d0c820d15074acb92455b8ac525bf6be9a0fe08c
snacksncode <snacksncode@gmail.com>
2021-01-22 12:53:19 +0100

    version update

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { Config } from "discord.js";
 const config: Config = {
   prefix: "s!",
   token: process.env.TOKEN,
-  version: "2.4.2",
+  version: "2.4.3",
   paths: {
     commands: "/commands/**/*.command.ts", //you can use pattern matching here
     events: "/events/**/*.event.ts",
